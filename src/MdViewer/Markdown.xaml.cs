b4274f0cc8934e10ccebfedff0fc5777ae6b22b2
masterLazy <msterlazy@qq.com>
2025-11-14 22:29:29 +0800

    feat: Develop basic functions

---
+++ src/MdViewer/Markdown.xaml.cs
@@ -0,0 +1,63 @@
+﻿/**
+   Copyright 2025 masterLazy
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+
+namespace MdViewer {
+    /// <summary>
+    /// MdViewer.xaml 的交互逻辑
+    /// </summary>
+    public partial class Markdown : UserControl, IDisposable {
+        private string _tempHtmlFile; // Temporary Html file
+
+        public new static readonly DependencyProperty ContentProperty =
+           DependencyProperty.Register("Content", typeof(string), typeof(Markdown));
+        public new string Content {
+            get { return (string)GetValue(ContentProperty); }
+            set {
+                SetValue(ContentProperty, value);
+                LoadMarkdown();
+            }
+        }
+
+        public Markdown() {
+            InitializeComponent();
+            _tempHtmlFile = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString() + ".html");
+            LoadMarkdown();
+
+            WebViewer.Navigating += (s, e) => {
+                var uri = e.Uri.ToString();
+                // Use default browser when navigating to internet
+                if (uri.StartsWith("https://") || uri.StartsWith("http://")) {
+                    e.Cancel = true;
+                    System.Diagnostics.Process.Start("explorer.exe", uri);
+                }
+            };
+        }
+
+        public void Dispose() {
+            if (File.Exists(_tempHtmlFile)) {
+                File.Delete(_tempHtmlFile);
+            }
+        }
+
+        private void LoadMarkdown() {
+            File.WriteAllText(_tempHtmlFile, MdConverter.ToHtml(Content));
+            WebViewer.Navigate(new Uri(_tempHtmlFile));
+        }
+    }
+}
